cb6a5d0709e2cfef23bbf2867ab4597d0bf5246a
teacher210 <teacher210@yandex.ru>
2021-12-04 12:01:16 +0400

    add auth

--- TavolgaAPI/Controllers/UserController.cs
+++ TavolgaAPI/Controllers/UserController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using TavolgaAPI.Core;
+using TavolgaAPI.Models;
+using TavolgaAPI.Models.Entityes.Users;
 
 namespace TavolgaAPI.Controllers
 {
@@ -11,6 +17,11 @@ namespace TavolgaAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+
+        private EfModel _dbContext;
+        public UserController(EfModel dbContext) {
+            _dbContext = dbContext;
+        }
         /// <summary>
         /// Может просматривать информацию о себе (ФИО участника, фото, оцениваемые параметры, результаты участника на предыдущих этапах);
         /// </summary>
@@ -25,13 +36,61 @@ namespace TavolgaAPI.Controllers
         [HttpPost("ChangeSelfPhoto")]
         public void ChangeSelfPhoto()
         {
+           /* var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity == null)
+                return BadRequest("User not Found!");
 
+            Convert.ToInt32(identity.FindFirst("UserId").Value);*/
         }
 
         [HttpPost]
-        public void Auth(string email, string password)
-        { 
-            
+        public object Auth(string email, string password)
+        {
+            var identity = GetIdentity(email, password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+
+            var now = DateTime.UtcNow;
+            var expies = now.Add(TimeSpan.FromDays(AuthOptions.LIFETIME));
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: expies,
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                expires = expies,
+                username = identity.Name
+            };
+
+            return response;
+        }
+
+        private ClaimsIdentity GetIdentity(string email, string password)
+        {
+            BaseUser user = _dbContext.BaseUsers.FirstOrDefault(x => x.Email == email && x.Password == password);
+            if (user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.FIO),
+                    new Claim("UserId", user.Id.ToString())
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            return null;
         }
     }
 }
